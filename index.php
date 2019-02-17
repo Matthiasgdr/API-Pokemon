26f4858d25b38c739811f803384f2ea02a14f0f4
Wizeeps <spam68130@gmail.com>
2019-02-17 22:13:16 +0100

    Yo

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
+    include('urls.php');
     
+    echo '<pre>';
+    print_r($results);
+    echo '</pre>';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Pokemon Searcher</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <form action="#" method="get">
+        <input type="text" name="pokemon" placeholder="Search a Pokemon">
+        <input value="Search" type="submit">
+    </form>
+    <?php foreach($results as $_pokemon): ?>
+        <div><?= $_pokemon->name ?></div>
+    <?php endforeach; ?>
+
+    <script src="script.js"></script>
+</body>
+</html>
